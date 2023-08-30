0c7098406a759d54ae1f573b681c17dd1176962a
root <root@LAPTOP-N1AEFKV0>
2023-08-30 17:53:19 -0400

    fixed broken route

--- node-express-1/node-express-1/broken-app/app.js
+++ node-express-1/node-express-1/broken-app/app.js
@@ -1,18 +1,47 @@
 const express = require('express');
 let axios = require('axios');
 var app = express();
+const ExpressError = require("./expressError")
+const bodyParser = require('body-parser')
 
-app.post('/', function(req, res, next) {
+app.use(bodyParser.json());
+
+// app.post('/', function(req, res, next) {
+//   try {
+//     let results = req.body.developers.map(async d => {
+//       return await axios.get(`https://api.github.com/users/${d}`);
+//     });
+//     let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+
+//     return res.json(out);
+//   } catch(err) {
+//     next(err);
+//   }
+// });
+
+
+// extract name and bio of developers from githubapi
+app.post('/', async (req, res, next)=>{
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-
-    return res.send(JSON.stringify(out));
-  } catch {
-    next(err);
+    const {developers} = req.body
+    const devs = developers
+    const data = [];
+    // loop over each dev and push data into array
+    for (const dev of devs) {
+      const res = await axios.get(`https://api.github.com/users/${dev}`)
+      data.push({
+        bio : res.data.bio,
+        name : res.data.name
+      })
+    }
+    // return jsonified data
+    return res.json({
+      data
+    })    
   }
-});
+  catch(err){
+    next(err)
+  }
+})
 
-app.listen(3000);
+module.exports = app;
--- node-express-1/node-express-1/broken-app/issues.md
+++ node-express-1/node-express-1/broken-app/issues.md
@@ -1,1 +1,8 @@
 # Broken App Issues
+
+try and catch but no actual error checker
+no err identifier after catch
+res.send(JSON.stringify(out)) is unnecassary replace with res.json(out)
+moved app.listen to a server.js file for testing and added a .log to tell devs which port the servers on
+    let results = req.body.developers.map is trying to loop over the developers array and return the information for each one however developers is left undefined at that point in execution.you would need to use Promise.all to await the resolution of all the promises.
+added comments for easier reading
