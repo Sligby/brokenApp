d693966ec89ec8a75f8c9cc631ed80a1ac19a427
root <root@LAPTOP-N1AEFKV0>
2023-09-01 10:30:20 -0400

    url http and https handling

--- node-express-1/node-express-1/urls.js
+++ node-express-1/node-express-1/urls.js
@@ -1,58 +1,99 @@
 const fs = require('fs');
+const http = require('http');
 const https = require('https')
-const urlModule = require('url')
+const urlModule = require('url');
 
-if (process.argv.length !==3){
-    console.error('Proper Form: node urls.js File')
+if (process.argv.length !=3) {
+    console.error('Operation is: node urls.js FILENAME')
     process.exit(1)
 }
 
-const inputFilename = process.argv[2]
 
-try{
-    const urls = fs.readFileSync(inputFilename, 'utf8').trim().split('\n');
-    const promises = [];
+function getURLdata(inputFile){
+    try{
+        const urls = fs.readFileSync(inputFile, 'utf8').trim().split('\n')
+        const promises = [];
 
-    for (const urlString of urls) {
-        const url = new URL(urlString);
-        const hostname = url.hostname;
+        for (const urlString of urls){
+            const url = new URL(urlString);
+            const hostname = url.hostname;
 
-        promises.push(new Promise((resolve, reject) => {
-            const request = https.get(url, response => {
-                if (response.statusCode !== 200){
-                    console.error('Couldnt download ${urlString}');
+            promises.push(new Promise((resolve, reject)=>{
+               if(url.protocol === 'http:') {
+                const request = http.get(url, response =>{
+                    if (response.statusCode !== 200 ){
+                        console.error(`couldnt download ${urlString}`)
+                        resolve()
+                        return;
+                    }
+
+                    let data = '';
+                    response.on('data', chunk => {
+                        data += chunk;
+                    });
+                    response.on('end', ()=>{
+                        const fileout = `${hostname}.txt`
+                        fs.writeFile(fileout, data, 'utf8', err => {
+                            if(err){
+                                console.error(`error writing to ${fileout}`)
+                            } 
+                            else{
+                                console.log(`file writen to ${hostname}`)
+                            }
+                            resolve();
+                        });
+                    });
+                });
+                request.on('error', err => {
+                    console.error(`Error downloading ${urlString}: ${err.message}`);
                     resolve();
-                    return;
-                }
-                let data = '';
-                response.on('data', chunk => {
-                    data += chunk;
+                });
+               }
+               else if (url.protocol === 'https:'){
+                const request = https.get(url, response =>{
+                    if (response.statusCode !== 200 ){
+                        console.error(`couldnt download ${urlString}`)
+                        resolve()
+                        return;
+                    }
+
+                    let data = '';
+                    response.on('data', chunk => {
+                        data += chunk;
+                    });
+                    response.on('end', ()=>{
+                        const fileout = `${hostname}.txt`
+                        fs.writeFile(fileout, data, 'utf8', err => {
+                            if(err){
+                                console.error(`error writing to ${fileout}`)
+                            } 
+                            else{
+                                console.log(`file writen to ${hostname}`)
+                            }
+                            resolve();
+                        });
+                    });
+                });
+                request.on('error', err => {
+                    console.error(`Error downloading ${urlString}: ${err.message}`);
+                    resolve();
                 });
 
-                response.on('end', () => {
-                    const outputFilename = '${hostname}.txt';
-                    fs.writeFile(outputFilename, data, 'utf8', err=> {
-                        if(err){
-                            console.error('Error writing to ${outputFilename}: ${err.message}');
-                        } else {
-                            console.log('wrote to ${hostname}')
-                        }
-                        resolve();
-                    })
-                })
-            })
-            
-            request.on('error', err =>{
-                console.error('error downloading ${urlString}: ${err.message}')
-                resolve();
-            });
-        }));
+               } else{
+                console.error(`Unsupported protocol: ${url.protocol}`)
+               }
+            }));
+        }
+        Promise.all(promises).then(()=>{
+            console.log('All downloads completed')
+        })
+    } catch(err){
+        console.error(`Error reading ${inputFile}: ${err.message}`)
     }
+}
 
-    Promise.all(promises),then(()=>{
-        console.log('all downloads completed');
-    });
-} 
-    catch (err){
-        console.error('Error reading ${inputFilename}: ${err.message}');
-    }
+
+const file = process.argv[2]
+getURLdata(file);
+
+module.exports = getURLdata;
